67a5fbf4e3234549507519ff9db64271dc59df07
hokageCV <chaitanyavaru@gmail.com>
2023-07-22 19:13:32 +0530

    add web component

--- app.js
+++ app.js
@@ -2,12 +2,59 @@ import { loadData } from "./services/Menu.js";
 import Router from "./services/Router.js";
 import Store from "./services/Store.js";
 
-window.kamui = {}
+// link web components       
+import { MenuPage } from "./components/MenuPage.js";
+import { OrderPage } from "./components/OrderPage.js";
+import { DetailsPage } from "./components/DetailsPage.js";
+
+window.kamui = {};
 kamui.store = Store;
 kamui.router = Router;
 
 // waiting for things to load before doing anything
-document.addEventListener('DOMContentLoaded', async ()=> {
-   loadData();
-   kamui.router.init();
+document.addEventListener("DOMContentLoaded", async () => {
+    loadData();
+    kamui.router.init();
+});
+
+
+window.kamui = {};
+kamui.store = Store;
+kamui.router = Router;
+
+// waiting for things to load before doing anything
+document.addEventListener("DOMContentLoaded", async () => {
+    loadData();
+    kamui.router.init();
+});
+
+
+window.kamui = {};
+kamui.store = Store;
+kamui.router = Router;
+
+// waiting for things to load before doing anything
+document.addEventListener("DOMContentLoaded", async () => {
+    loadData();
+    kamui.router.init();
+});
+
+window.kamui = {};
+kamui.store = Store;
+kamui.router = Router;
+
+// waiting for things to load before doing anything
+document.addEventListener("DOMContentLoaded", async () => {
+    loadData();
+    kamui.router.init();
+});
+
+window.kamui = {};
+kamui.store = Store;
+kamui.router = Router;
+
+// waiting for things to load before doing anything
+document.addEventListener("DOMContentLoaded", async () => {
+    loadData();
+    kamui.router.init();
 });
--- services/Router.js
+++ services/Router.js
@@ -26,16 +26,15 @@ const Router = {
         let pageElement = null;
         switch (route) {
             case "/":
-                pageElement = document.createElement("h1");
-                pageElement.textContent = "Menu";
+                pageElement = document.createElement("menu-page");
                 break;
             case "/order":
-                pageElement = document.createElement("h1");
+                pageElement = document.createElement("order-page");
                 pageElement.textContent = "Your Order";
                 break;
             default:
                 if (route.startsWith("/product-")) {
-                    pageElement = document.createElement("h1");
+                    pageElement = document.createElement("details-page");
                     pageElement.textContent = "Product Details";
 
                     const paramId = route.substring(route.lastIndexOf("-") + 1);
