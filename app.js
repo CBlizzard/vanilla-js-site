74f6800f590132033b71a5bf4f3146828cb35fd9
hokageCV <chaitanyavaru@gmail.com>
2023-07-22 17:01:13 +0530

    add router

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
 import Store from "./services/Store.js";
 
 window.kamui = {}
-kamui.store = Store
+kamui.store = Store;
+kamui.router = Router;
 
 // waiting for things to load before doing anything
 document.addEventListener('DOMContentLoaded', async ()=> {
    loadData();
+   kamui.router.init();
 });
---
+++ services/Router.js
@@ -0,0 +1,59 @@
+const Router = {
+    init: () => {
+        document.querySelectorAll("a.navlink").forEach((a) => {
+            a.addEventListener("click", (e) => {
+                e.preventDefault();
+                // const url = e.target.href;
+                const url = e.target.getAttribute("href");
+                Router.go(url);
+            });
+        });
+
+        // event listener for URL changes
+        window.addEventListener("popstate", (e) => {
+            Router.go(e.state.route, false); // we added 'route' object at history.pushState() below
+        });
+
+        // check initial URL
+        Router.go(location.pathname);
+    },
+    go: (route, addToHistory = true) => {
+        console.log(`going to ${route}`);
+        if (addToHistory) {
+            history.pushState({ route }, "", route);
+        }
+
+        let pageElement = null;
+        switch (route) {
+            case "/":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Menu";
+                break;
+            case "/order":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Your Order";
+                break;
+            default:
+                if (route.startsWith("/product-")) {
+                    pageElement = document.createElement("h1");
+                    pageElement.textContent = "Product Details";
+
+                    const paramId = route.substring(route.lastIndexOf("-") + 1);
+                    pageElement.dataset.id = paramId;
+                }
+        }
+
+        if (pageElement) {
+            // document.querySelector("main").children[0].remove();
+            let cache = document.querySelector("main");
+
+            cache.innerHTML = "";
+            cache.appendChild(pageElement);
+
+            window.scrollX = 0;
+            window.scrollY = 0;
+        }
+    },
+};
+
+export default Router;
